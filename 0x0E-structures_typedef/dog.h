912180ccad12230aa81477d67ee1ea2c6fab8d33
EdemDavid <daviddufe9@gmail.com>
2023-11-07 02:03:53 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,23 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
-struct dog {
+/**
+ * struct dog - Defines a structure named dog
+ * @name: dog name denoted
+ * @owner: person who owns dog
+ * @age: dog's age
+ *
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
